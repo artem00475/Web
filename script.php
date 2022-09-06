9e1cc44c64ff8c41b4b1021a61c78b59946d3060
Artem <tuchin12345678artem@gmail.com>
2022-09-06 20:21:39 +0300

    Валидация на сервере

--- script.php
+++ script.php
@@ -6,6 +6,24 @@
     $timeOffset = $_POST['time']*(-1);
     $hit;
 
+    function validateY($y) {
+        if (preg_match("/^-?[0-4](\.[0-9]*)?$/",$y)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    function validateR($r) {
+        if (preg_match("/^[1-5]$/",$r)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     function ifFirstQuerter($x,$y,$r) {
         if ($x >= 0 && $y >= 0 && ($x**2+$y**2 <= $r**2)) return true;
         return false;
@@ -18,6 +36,7 @@
         if ($x <= 0 && $y <= 0 && $x >= -$r/2 && $y >= -$r) return true;
         return false;
     }
+    if (validateY($y) && validateR($r)) {
     if (ifFirstQuerter($x,$y,$r) || ifSecondQuerter($x,$y,$r) || ifThirdQuerter($x,$y,$r)) $hit="Да";
     else $hit="Нет";
     $date = Date('H:i:s',strtotime($timeOffset.' minutes'));
@@ -28,5 +47,6 @@
     $count++;
     setcookie("count",$count,time()+300);
     setcookie("table".$count,$tableArray,(time()+300));
+    }
     header('Location: table.php')
     ?>
